43b37f5d3bf527578ed6985149696061ccc21616
ibrahima <lodiajames@hotmail.com>
2023-02-02 16:45:30 -0500

    styling card component

--- components/Card.js
+++ components/Card.js
@@ -5,14 +5,14 @@ import { HandThumbUpIcon} from '@heroicons/react/24/outline'
 export default function Card({result}) {
     const ROOT_URL = "https://image.tmdb.org/t/p/original/"
   return (
-    <div>
+    <div className='p-3 hover:text-gray-200 cursor-pointer active:text-gray-400 xl:hover:scale-105 transition-transform duration-200'>
         <Image alt='trending pic'  layout='responsive' src={ROOT_URL + result.backdrop_path}  width={200} height={200}/>
          
-          <div className=''>
-               <p className='truncate'> {result.overview}</p> 
-               <h2>{result.title || result.name}</h2>
-               <p>{result.release_date || result.first_air_date}
-               <HandThumbUpIcon className='h-5'/>
+          <div className='p-2'>
+               <p className='truncate text-lg'> {result.overview}</p> 
+               <h2 className='text-lg font-bold'>{result.title || result.name}</h2>
+               <p className='flex items-center'>{result.release_date || result.first_air_date}
+               <HandThumbUpIcon className='h-5 ml-3 mr-1' />
                {result.vote_count}
                </p>
 
--- components/Results.js
+++ components/Results.js
@@ -3,7 +3,7 @@ import Card from './Card'
 
 export default function Results({results}) {
   return (
-    <div>
+    <div className='bg-[#121212] text-white select-none sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5'>
       {results.map(result=>(
         <Card key={result.id} result={result}/>
       ))}
